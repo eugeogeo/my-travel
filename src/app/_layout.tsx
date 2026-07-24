cc16ceffcb50cb2f03eebc56a6763a19ab9a58b3
Geovana Figueiredo Silva <geovanaf1912@gmail.com>
2026-07-23 21:05:37 -0300

    refactor: replace AppTabs with Stack navigation in RootLayout

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -3,17 +3,19 @@ import {
   DefaultTheme,
   ThemeProvider,
 } from "@react-navigation/native";
+import { Stack } from "expo-router";
+import React from "react";
 import { useColorScheme } from "react-native";
 import "../global.css";
-
-import AppTabs from "../components/app-tabs";
 
 export default function RootLayout() {
   const colorScheme = useColorScheme();
 
   return (
     <ThemeProvider value={colorScheme === "dark" ? DarkTheme : DefaultTheme}>
-      <AppTabs />
+      <Stack screenOptions={{ headerShown: false }}>
+        <Stack.Screen name="index" />
+      </Stack>
     </ThemeProvider>
   );
 }
